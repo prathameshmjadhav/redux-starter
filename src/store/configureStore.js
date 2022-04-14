20210dc748a9a1d498b8aa9f77bdb68efdc5927e
Prathamesh <prathameshmjadhav@gmail.com>
2022-04-14 13:50:04 +0530

    refactor: Add trace in redux dev tool. (Not Working).

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,8 @@
 import { createStore } from 'redux';
 import reducer from './bugs';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 export default function configureStore() {
-  const store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
+  const store = createStore(reducer, devToolsEnhancer({ trace: true }));
   return store;
 }
