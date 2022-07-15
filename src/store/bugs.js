90423e990237d2606dd52ecf7619805ff1038991
Prathamesh <prathameshmjadhav@gmail.com>
2022-07-15 09:39:28 +0530

    feat: Add createAction from React Toolkit

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,4 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
 let lastId = 0;
 
@@ -8,27 +8,46 @@ export const bugResolved = createAction('bugResolved');
 
 //Reducers or action Handlers
 
-export default function reducer(state = [], action) {
-  switch (action.type) {
-    case bugAdded.type:
-      return [
-        ...state,
-        {
-          id: ++lastId,
-          description: action.payload.description,
-          resolved: false,
-        },
-      ];
-
-    case bugRemoved.type:
-      return state.filter((bug) => bug.id !== action.payload.id);
-
-    case bugResolved.type:
-      return state.map((bug) =>
-        bug.id === action.payload.id ? { ...bug, resolved: true } : bug
-      );
-
-    default:
-      return state;
+ export default createReducer(
+  [],
+  {[bugAdded.type]:(bugs, action) => {
+    bugs.push({
+      id:++lastId,
+      description:action.payload.description,
+      resolved: false
+    });
+    },
+  
+  
+    [bugResolved.type]: (bugs, action) => {
+      const index = bugs.findIndex(bug => bug.id === action.payload.id)
+      bugs[index].resolved = true
+    }
   }
-}
+)
+
+
+// export default function reducer(state = [], action) {
+//   switch (action.type) {
+//     case bugAdded.type:
+//       return [
+//         ...state,
+//         {
+//           id: ++lastId,
+//           description: action.payload.description,
+//           resolved: false,
+//         },
+//       ];
+
+//     case bugRemoved.type:
+//       return state.filter((bug) => bug.id !== action.payload.id);
+
+//     case bugResolved.type:
+//       return state.map((bug) =>
+//         bug.id === action.payload.id ? { ...bug, resolved: true } : bug
+//       );
+
+//     default:
+//       return state;
+//   }
+// }
